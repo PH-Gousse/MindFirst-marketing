fe36e8405ee378e64fbbabb9980f9d11d64fcbaa
Pierre-Henri Gousse <pierre.gousse@thirdbridge.com>
2021-10-27 16:59:19 +0100

    Trigger Build

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+          Mind First
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
